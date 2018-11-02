dd7802b9d796aafd63c98b2df0520b53b06d6a86
EC2 Default User <ec2-user@ip-172-31-37-125.ec2.internal>
2018-11-02 16:10:16 +0000

    node not working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/whiteboard', function (req, res, next) {
+    var url = "https://owlbot.info/api/v1/dictionary/";
+    console.log("query ",req.query);
+    url += req.query['q'];
+    url += "?format=json";
+    request(url).pipe(res);
 });
 
 module.exports = router;
